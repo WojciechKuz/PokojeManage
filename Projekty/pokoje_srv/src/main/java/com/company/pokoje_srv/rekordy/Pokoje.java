188745373d48ad407b07c2271d5840e0b55e0e8f
WojciechKuz <104651306+WojciechKuz@users.noreply.github.com>
2022-09-11 13:00:24 +0200

    Dodałem adnotacje do Pokoje.java
    Adnotacje, żeby Spring mógł stworzyć tabelę w bazie danych

--- Projekty/pokoje_srv/src/main/java/com/company/pokoje_srv/rekordy/Pokoje.java
+++ Projekty/pokoje_srv/src/main/java/com/company/pokoje_srv/rekordy/Pokoje.java
@@ -1,40 +1,71 @@
 package com.company.pokoje_srv.rekordy;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+//import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import java.util.List;
+
+@Entity
+@Table(name="pokoje")
 public class Pokoje {
-    int id; // ? id ? tak, ale nie trzeba wyświetlać
-    int nr_pokoju;
-    int budynek; // adres czy numer budynku?
-    String nr_tel;
-    String przeznaczenie;
-    int ile_biurek;
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    private Integer nr_pokoju;
+    private String budynek; // adres
+    private String nr_tel;
+    private String przeznaczenie;
+    private Integer ile_biurek;
+    @OneToMany(mappedBy ="pokoje")
+    private List<Biurka> biurka;
 
-    public Pokoje(int id, int nr_pokoju, int budynek, String nr_tel, String przeznaczenie, int ile_biurek) {
-        this.id = id;
-        this.nr_pokoju = nr_pokoju;
-        this.budynek = budynek;
-        this.nr_tel = nr_tel;
-        this.przeznaczenie = przeznaczenie;
-        this.ile_biurek = ile_biurek;
+    public Integer getId() {
+        return id;
     }
-
-    public int getNr_pokoju() {
+    public Integer getNr_pokoju() {
         return nr_pokoju;
     }
-
-    public int getBudynek() {
+    public String getBudynek() {
         return budynek;
     }
-
     public String getNr_tel() {
         return nr_tel;
     }
-
     public String getPrzeznaczenie() {
         return przeznaczenie;
     }
-
-    public int getIle_biurek() {
+    public Integer getIle_biurek() {
         return ile_biurek;
+    }
+    public List<Biurka> getBiurka() {
+        return biurka;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public void setNr_pokoju(Integer nr_pokoju) {
+        this.nr_pokoju = nr_pokoju;
+    }
+    public void setBudynek(String budynek) {
+        this.budynek = budynek;
+    }
+    public void setNr_tel(String nr_tel) {
+        this.nr_tel = nr_tel;
+    }
+    public void setPrzeznaczenie(String przeznaczenie) {
+        this.przeznaczenie = przeznaczenie;
+    }
+    public void setIle_biurek(Integer ile_biurek) {
+        this.ile_biurek = ile_biurek;
+    }
+    public void setBiurka(List<Biurka> biurka) {
+        this.biurka = biurka;
     }
 
     @Override
