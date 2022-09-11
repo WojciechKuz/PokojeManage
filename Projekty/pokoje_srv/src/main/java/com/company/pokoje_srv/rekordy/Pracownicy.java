83629688d9951e6e6b6832a76a47e64ad13b85ae
WojciechKuz <104651306+WojciechKuz@users.noreply.github.com>
2022-09-11 13:06:44 +0200

    upload poprawionych klas w 'rekordach'
    Klasy Biurka, Pokoje, Pracownicy mapujące tabele BD

--- Projekty/pokoje_srv/src/main/java/com/company/pokoje_srv/rekordy/Pracownicy.java
+++ Projekty/pokoje_srv/src/main/java/com/company/pokoje_srv/rekordy/Pracownicy.java
@@ -1,52 +1,78 @@
 package com.company.pokoje_srv.rekordy;
 
-import java.sql.Date;   // java.sql.Date albo java.util.Date ?
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+//import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
+import java.util.List;
+import java.util.Date;
+
+@Entity
+@Table(name="pracownicy")
 public class Pracownicy {
-    int id;
-    String imie;
-    String nazwisko;
-    String nr_tel_kom;
-    String rodzaj_pracown;
-    Date zatrudnienie_od;
-    Date zatrudnienie_do;
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    private String imie;
+    private String nazwisko;
+    private String nr_tel_kom;
+    private String rodzaj_pracown;
+    private Date zatrudnienie_od;
+    private Date zatrudnienie_do;
+    @OneToMany(mappedBy ="pracownicy")
+    private List<Biurka> biurka;
 
-    public Pracownicy(int id, String imie, String nazwisko, String nr_tel_kom,
-                      String rodzaj_pracown, Date zatrudnienie_od, Date zatrudnienie_do) {
-        this.id = id;
-        this.imie = imie;
-        this.nazwisko = nazwisko;
-        this.nr_tel_kom = nr_tel_kom;
-        this.rodzaj_pracown = rodzaj_pracown;
-        this.zatrudnienie_od = zatrudnienie_od;
-        this.zatrudnienie_do = zatrudnienie_do;
-    }
-
-    public int getId() {
+    public Integer getId() {
         return id;
     }
-
     public String getImie() {
         return imie;
     }
-
     public String getNazwisko() {
         return nazwisko;
     }
-
     public String getNr_tel_kom() {
         return nr_tel_kom;
     }
-
     public String getRodzaj_pracown() {
         return rodzaj_pracown;
     }
-
     public Date getZatrudnienie_od() {
         return zatrudnienie_od;
     }
-
     public Date getZatrudnienie_do() {
         return zatrudnienie_do;
+    }
+    public List<Biurka> getBiurka() {
+        return biurka;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+    public void setNr_tel_kom(String nr_tel_kom) {
+        this.nr_tel_kom = nr_tel_kom;
+    }
+    public void setRodzaj_pracown(String rodzaj_pracown) {
+        this.rodzaj_pracown = rodzaj_pracown;
+    }
+    public void setZatrudnienie_od(Date zatrudnienie_od) {
+        this.zatrudnienie_od = zatrudnienie_od;
+    }
+    public void setZatrudnienie_do(Date zatrudnienie_do) {
+        this.zatrudnienie_do = zatrudnienie_do;
+    }
+    public void setBiurka(List<Biurka> biurka) {
+        this.biurka = biurka;
     }
 }
--- Projekty/pokoje_srv/src/main/java/com/company/pokoje_srv/rekordy/WolnePokoje.java
+++ Projekty/pokoje_srv/src/main/java/com/company/pokoje_srv/rekordy/WolnePokoje.java
@@ -2,40 +2,44 @@ package com.company.pokoje_srv.rekordy;
 
 public class WolnePokoje {
     /*
-    wynik zapytania sql
+        Nie tabela w BD, tylko do przesyłania
      */
 
-    int nr_pokoju;
-    int budynek;
+    Integer nr_pokoju;
+    String budynek; // adres
     String przeznaczenie;
-    int ile_wolnych;    // ile jest wolnych biurek
-    int ile_biurek;
-
-    public WolnePokoje(int nr_pokoju, int budynek, String przeznaczenie, int ile_wolnych, int ile_biurek) {
-        this.nr_pokoju = nr_pokoju;
-        this.budynek = budynek;
-        this.przeznaczenie = przeznaczenie;
-        this.ile_wolnych = ile_wolnych;
-        this.ile_biurek = ile_biurek;
-    }
+    Integer ile_wolnych;    // ile jest wolnych biurek
+    Integer ile_biurek;
 
     public int getNr_pokoju() {
         return nr_pokoju;
     }
-
-    public int getBudynek() {
+    public String getBudynek() {
         return budynek;
     }
-
     public String getPrzeznaczenie() {
         return przeznaczenie;
     }
-
     public int getIle_wolnych() {
         return ile_wolnych;
     }
-
     public int getIle_biurek() {
         return ile_biurek;
+    }
+
+    public void setNr_pokoju(Integer nr_pokoju) {
+        this.nr_pokoju = nr_pokoju;
+    }
+    public void setBudynek(String budynek) {
+        this.budynek = budynek;
+    }
+    public void setPrzeznaczenie(String przeznaczenie) {
+        this.przeznaczenie = przeznaczenie;
+    }
+    public void setIle_wolnych(Integer ile_wolnych) {
+        this.ile_wolnych = ile_wolnych;
+    }
+    public void setIle_biurek(Integer ile_biurek) {
+        this.ile_biurek = ile_biurek;
     }
 }
