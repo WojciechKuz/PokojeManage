ade65b7fe8b6de085a75f717429fd759a3a2b6e3
WojciechKuz <104651306+WojciechKuz@users.noreply.github.com>
2022-09-12 16:43:23 +0200

    dostęp do bazy danych
    get____(),
    addRecord(),
    editRecord(),
    deleteRecord()

--- Projekty/pokoje_srv/src/main/java/com/company/pokoje_srv/WebController.java
+++ Projekty/pokoje_srv/src/main/java/com/company/pokoje_srv/WebController.java
@@ -92,6 +92,141 @@ public class WebController
 
 
 
+    // // // // // DOSTĘP DO DANYCH // // // // //
+
+    // GET
+    // gdy zamiast RestController Controller, to przed typem zwracanym @ResponseBody
+    //@GetMapping("/pokoje")
+    public Pokoje[] getPokoje(){
+        ArrayList<Pokoje> tab = (ArrayList<Pokoje>) pokojeRepo.findAll();
+        return tab.toArray(new Pokoje[tab.size()]);
+    }
+    //@GetMapping("/pracownicy")
+    public  Pracownicy[] getPracownicy(){
+        ArrayList<Pracownicy> tab = (ArrayList<Pracownicy>) pracownicyRepo.findAll();
+        return tab.toArray(new Pracownicy[tab.size()]);
+    }
+    //@GetMapping("/biurka")
+    public Biurka[] getBiurka(){
+        ArrayList<Biurka> tab = (ArrayList<Biurka>) biurkaRepo.findAll();
+        return tab.toArray(new Biurka[tab.size()]);
+    }
+    //@GetMapping("/wolne")
+    public WolnePokoje[] getWolne() {
+
+        // zwrócić dane ile jest jeszcze wolnych biurek w pokojach (tablica)
+        //
+        System.out.println("UWAGA! \"GET /wolne\" nie jest jeszcze obsługiwane.");
+        return new WolnePokoje[1]; // tymczasowe
+    }
+
+    Integer addRecord(Pokoje pok) {
+        if(pok == null)
+            return 1;
+        pok.setId(null);
+        pokojeRepo.save(pok);
+        return 0;
+    }
+    Integer addRecord(Pracownicy pra) {
+        if(pra == null)
+            return 1;
+        pra.setId(null);
+        pracownicyRepo.save(pra);
+        return 0;
+    }
+    Integer addRecord(Biurka biu) {
+        if(biu == null)
+            return 1;
+        biu.setIdb(null);
+        biurkaRepo.save(biu);
+        return 0;
+    }
+    Integer editRecord(Pokoje pok) {
+        if(pok == null)
+            return 1;
+        pokojeRepo.save(pok);
+        return 0;
+    }
+    Integer editRecord(Pracownicy pra) {
+        if(pra == null)
+            return 1;
+        pracownicyRepo.save(pra);
+        return 0;
+    }
+    Integer editRecord(Biurka biu) {
+        if(biu == null)
+            return 1;
+        biurkaRepo.save(biu);
+        return 0;
+    }
+
+
+
+    // POST - dodawanie i edycja
+    @PostMapping("/pokoje/{mode}")
+    Integer postRecord(@RequestBody Pokoje pok, @PathVariable String mode) {
+        int rv = 0;
+        switch(mode) {
+            case "add" -> {
+                rv = addRecord(pok);
+            }
+            case "edit" -> {
+                rv = editRecord(pok);
+            }
+        }
+        return rv;
+    }
+    @PostMapping("/pracownicy/{mode}")
+    Integer postRecord(@RequestBody Pracownicy pra, @PathVariable String mode) {
+        int rv = 0;
+        switch(mode) {
+            case "add" -> {
+                rv = addRecord(pra);
+            }
+            case "edit" -> {
+                rv = editRecord(pra);
+            }
+        }
+        return rv;
+    }
+    @PostMapping("/biurka/{mode}")
+    Integer postRecord(@RequestBody Biurka biu, @PathVariable String mode) {
+        int rv = 0;
+        switch(mode) {
+            case "add" -> {
+                rv = addRecord(biu);
+            }
+            case "edit" -> {
+                rv = editRecord(biu);
+            }
+        }
+        return rv;
+    }
+
+    // PUT NIE MA! zamiast tego używać POST /{tabela}/edit
+
+
+    // DELETE
+    @DeleteMapping("/{table}/{id}")
+    void deleteRecord(@PathVariable String table, @PathVariable Integer id){
+        switch(table) {
+            case "pokoje" -> pokojeRepo.deleteById(id);
+            case "pracownicy" -> pracownicyRepo.deleteById(id);
+            case "biurka" -> biurkaRepo.deleteById(id);
+        }
+    }
+
+    /* klient może wysłać:
+    [WYŁĄCZONE] get - otrzymaj dane tabeli		/tabela
+    post - dodaj lub edytuj rekord	/tabela/add	 /tabela/edit
+    delete - usuń rekord			/tabela/id
+
+    (nazwa tabeli w requescie małymi literami)
+
+    put - nie ma, bo nie zwraca kodu
+     */
+
+
 }
 
 
