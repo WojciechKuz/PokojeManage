4f4809e7657d4553c20351072a6ac1944993a587
Wojtek <wojtek.kuzbinski@gmail.com>
2022-09-13 09:50:39 +0200

    plik z procedurą

---
+++ Projekty/procedury.sql
@@ -0,0 +1,23 @@
+
+ -- procedura usuwająca pracowników
+CREATE DEFINER=`springuser`@`%` PROCEDURE `del_pracownicy`()
+LANGUAGE SQL
+NOT DETERMINISTIC
+CONTAINS SQL
+SQL SECURITY DEFINER
+COMMENT ''
+BEGIN
+	DELETE
+	FROM pracownicy
+	WHERE TIMESTAMPDIFF(DAY, 'zatrudnienie_do', NOW());
+END
+ -- zdarzenie
+CREATE EVENT `del_event`
+	ON SCHEDULE
+		EVERY 1 HOUR
+	ON COMPLETION NOT PRESERVE
+	ENABLE
+	COMMENT ''
+	DO BEGIN
+	CALL del_pracownicy();
+END
